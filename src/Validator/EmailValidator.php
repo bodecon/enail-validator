f950a6a744f09b175469811febf7f2bfcf832ce1
Kirill <kirill.kirillov@itglobal.com>
2022-03-02 15:27:34 +0300

    fix email validation

--- src/Validator/EmailValidator.php
+++ src/Validator/EmailValidator.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Kirillov\EmailValidator\Validator;
 
 use Kirillov\EmailValidator\Storage\HostsStorage;
+use InvalidArgumentException;
 
 class EmailValidator
 {
@@ -12,15 +13,25 @@ class EmailValidator
         private HostsStorage $hostsStorage
     ) { }
 
+    /**
+     * @param string $email
+     * @return bool
+     *
+     * @throws
+     */
     public function isValid(string $email): bool
     {
         if (empty($email)) {
             throw new InvalidArgumentException('Email is empty.');
         }
 
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException('Email is invalid.');
+        }
+
         $host = explode('@', $email)[1];
         $validHosts = $this->hostsStorage->getAll();
 
-        return filter_var($email, FILTER_VALIDATE_EMAIL) && getmxrr($host,$validHosts);
+        return in_array($host,$validHosts);
     }
 }
